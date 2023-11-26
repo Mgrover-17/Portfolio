87eadc56f65c37d2a24c4c62c974f7452ccc8178
Manvi Grover <101446386+Mgrover-17@users.noreply.github.com>
2023-11-27 01:17:31 +0530

    Projects and Achievement Added

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,6 +3,9 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import BookHub from "../assets/img/BookHub.png";
+import JobPortral from "../assets/img/jobPortral.png"
+import TechStar from "../assets/img/TechStars.jpg"
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -28,7 +31,29 @@ export const Projects = () => {
       imgUrl: projImg3,
       url:"https://project-car-dekho-manvi-grover.vercel.app/",
     },
-    
+  ];
+
+  const projects2 = [
+    {
+      title: "Book Hub",
+      description: "FEE St-Project on API",
+      imgUrl: BookHub,
+      url:"https://github.com/Mgrover-17/BookSearch_API",
+    },    
+    {
+      title: "Job Portral",
+      description: "Job Portal Using HTML,CSS and PHP",
+      imgUrl:JobPortral,
+      url:"https://github.com/Mgrover-17/Job-Portral-Project",
+    },    
+  ];
+  const Achievements = [
+    {
+      title: "TechStars Startup Weekend",
+      description: "Got 3rd Prize in TechStars Startup Weekend Women Chandigarh",
+      imgUrl: TechStar,
+      url:"https://www.linkedin.com/posts/manvi-grover-60305623b_keeplearning-techstars-googleforstartups-activity-6976894220724776960-osCw?utm_source=share&utm_medium=member_desktop",
+    },       
   ];
 
   return (
@@ -38,8 +63,8 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate_animated animate_fadeIn": ""}>
-                <h2>Projects</h2>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects and Achievements</h2>
                 <p>Click To Explore the Projects</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
@@ -47,13 +72,13 @@ export const Projects = () => {
                       <Nav.Link eventKey="first">Tab 1</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      <Nav.Link eventKey="second">2</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">Achievements</Nav.Link>
                     </Nav.Item>
                   </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate_animated animate_slideInUp" : ""}>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
                       <Row>
                         {
@@ -69,10 +94,32 @@ export const Projects = () => {
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                      <p>Projects</p>
+                    <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Projects</p>
+                    <Row>
+                        {
+                          Achievements.map((ach, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...ach}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
@@ -81,6 +128,6 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <image className="background-image-right" src={colorSharp2}></image>
+      <img className="background-image-right" src={colorSharp2}></img>
     </section>
   )}
